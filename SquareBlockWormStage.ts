eeb8dc60e706341741163b1c29e11651bdafa2ab
Anwesh43 <anweshthecool0@gmail.com>
2019-03-11 10:49:29 +0530

    adding SquareBlockWorm class

--- SquareBlockWormStage.ts
+++ SquareBlockWormStage.ts
@@ -167,3 +167,26 @@ class SBWNode {
         return this
     }
 }
+
+class SquareBlockWorm {
+
+    curr : SBWNode = new SBWNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
