62893b101b05074d237044ce8ef5486f88937e9c
Anwesh43 <anweshthecool0@gmail.com>
2019-03-11 10:46:57 +0530

    adding SBWNode class

--- SquareBlockWormStage.ts
+++ SquareBlockWormStage.ts
@@ -125,3 +125,45 @@ class Animator {
         }
     }
 }
+
+class SBWNode {
+
+    prev : SBWNode
+    next : SBWNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new SBWNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawSBWNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SBWNode {
+        var curr : SBWNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
