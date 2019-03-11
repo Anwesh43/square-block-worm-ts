9ab2591fd31058281b3f89b8451f565e8cfff37c
Anwesh43 <anweshthecool0@gmail.com>
2019-03-11 10:30:21 +0530

    adding State class

--- SquareBlockWormStage.ts
+++ SquareBlockWormStage.ts
@@ -58,3 +58,26 @@ class SquareBlockWormStage {
         stage.handleTap()
     }
 }
+
+class State {
+    scale : number = 0
+    dir : number = 0
+    prevScale : number = 0
+
+    update(cb : Function) {
+        this.scale += updateValue(this.scale, this.dir, squares, 1)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb(this.prevScale)
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
