a76b4d96afd3deb94c58b0d181808282a70bedb8
Anwesh43 <anweshthecool0@gmail.com>
2019-03-11 10:56:00 +0530

    adding renderer in stage

--- SquareBlockWormStage.ts
+++ SquareBlockWormStage.ts
@@ -56,6 +56,7 @@ const drawSBWNode : Function = (context : CanvasRenderingContext2D, i : number, 
 class SquareBlockWormStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w
@@ -67,11 +68,14 @@ class SquareBlockWormStage {
     render() {
         this.context.fillStyle = backColor
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
