26640595a5cb0f01339f491eef0ea13843a71173
Anwesh43 <anweshthecool0@gmail.com>
2019-03-11 10:28:31 +0530

    adding scale methods

--- SquareBlockWormStage.ts
+++ SquareBlockWormStage.ts
@@ -10,6 +10,25 @@ const rSizeFactor : number = 3
 const foreColor : string = "#4CAF50"
 const backColor : string = "#BDBDBD"
 
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1 / n, maxScale(scale, i, n)) * n
+}
+
+const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
+
+const mirrorValue : Function = (scale : number, a : number, b : number) => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const updateValue : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
+
 class SquareBlockWormStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
